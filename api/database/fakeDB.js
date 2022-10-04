adcc6e6a4fd292382f73c68fdc3f54e44bd399bd
JohanGJensen <johancentralen@gmail.com>
2022-10-04 20:15:31 +0200

    update fake db content

--- api/database/fakeDB.js
+++ api/database/fakeDB.js
@@ -5,9 +5,9 @@ const fakeCollection = [
     tags: "Tripel - Belgian Style",
     image: "genesis_placeholder.png",
     apv: "7.9%",
-    ingredients: ["pilsner malt", "Styrian Goldings hops", "Hallertauer hops"],
-    smells: ["floral", "fruity"],
-    tastes: ["caramel", "sweet", "yeasty"],
+    ingredients: ["Pilsner Malt", "Styrian Goldings Hops", "Hallertauer Hops"],
+    smells: ["Floral", "Fruity"],
+    tastes: ["Caramel", "Sweet", "Yeasty"],
     color: {
       label: "Amber Golden",
       // hexValues: ["#f6c101", "#df8d03", "#c96e12"],
